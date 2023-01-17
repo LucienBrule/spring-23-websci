13862a234995a2013d81c564684ecc75e2e05886
Lucien Brule <lucien@lucienbrule.com>
2023-01-17 09:07:43 -0500

    lab 1

--- lab-1/src/pages/_document.tsx
+++ lab-1/src/pages/_document.tsx
@@ -1,10 +1,21 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Link from "next/link";
 
+
+function Navigation(){
+    return (
+        <div className={"navigation"}>
+            <h3>NewsTicker</h3>
+            <Link href="/">Home</Link>
+        </div>
+    )
+}
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <Navigation/>
         <Main />
         <NextScript />
       </body>
--- lab-1/src/pages/index.tsx
+++ lab-1/src/pages/index.tsx
@@ -1,134 +1,103 @@
-import {useEffect, useState} from "react";
-
-type ItemId = number;
-
-interface IndexState {
-    items: Array<number>
-}
+import {useState} from "react";
 
 interface Item {
     by: string,
     id: number,
     kids: Array<number>,
-    "score": number,
-    "time": number,
-    "title": string,
-    "type": string,
-    "url": string
+    score: number,
+    time: number,
+    title: string,
+    type: string,
+    url: string
 
 }
 
-
-function ItemComponent(props: Item) {
-    return <div>
-        <h3 style={{'lineHeight': '1rem'}}>{props.title}</h3>
-        <span style={{
-            'display': 'flex',
-            'alignItems': 'baseline',
-        }}>
-            <div style={{
-                'marginRight': '1rem',
-                'width': '5rem',
-            }}
-            >
-                {props.by}
-            </div>
-            <div className={'item-score'}>{props.score}</div>
-            <p className={'item-time'}>{props.time}</p>
-            <a className={'item-url'}
-               href={props.url}
-            >{props.url}</a>
-        </span>
-    </div>
-
+interface NewsFeedProps {
+    itemIds: Array<number>;
+    items: Array<Item>;
 }
 
-function ItemHOC(props: { item: Item }) {
 
-    let [self, setSelf] = useState(props.item);
+export async function getStaticProps() {
+    const res = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json');
+    const itemIds: Array<number> = await res.json();
+    let items: Array<Item> = [];
 
-    useEffect(() => {
-        console.log("ItemComponent mounted");
-        console.log(props.item);
+    // slice for demo
+    for (let itemId of itemIds.slice(0,50)) {
+        const res = await fetch(`https://hacker-news.firebaseio.com/v0/item/${itemId}.json`);
+        const item: Item = await res.json();
+        items.push(item);
+    }
 
-        let fetchSelf = async () => {
-
-            try {
-
-                let response = await fetch("https://hacker-news.firebaseio.com/v0/item/" + props.item.id + ".json");
-                let data = await response.json();
-                console.log(data);
-                setSelf(data);
-            } catch (e) {
-                console.log(e);
-            }
-        }
-
-        fetchSelf();
-    }, [props.item])
-
-
-    return <ItemComponent {...self}/>
-}
-
-function Navbar() {
-    return <div className={".navigation"}>
-
-    </div>;
-}
-
-function Items(props: { state: { items: Array<number> | undefined }, element: (itemId: ItemId) => JSX.Element }) {
-    return <div className={"Home.content"}>
-        {
-            props.state?.items?.map(props.element)
-        }
-    </div>;
-}
-
-export default function Index() {
-
-    let [state, setState] = useState<IndexState>({items: []});
-    let deserializeItemsFromResponse = async (response: Response) => {
-        try {
-            let items: Array<number> = await response.json()
-            setState({...state, items})
-        } catch (e) {
-            console.error("Error parsing JSON. ", e)
+    return {
+        props: {
+            itemIds,
+            items
         }
     }
 
-    let fetchItems = async () => {
-        try {
-            console.log("fetching items")
-            let response = await fetch("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
-            await deserializeItemsFromResponse(response)
-        } catch (e) {
-            console.error("Failed to fetch: ", e)
-            throw e
-        }
-    }
+}
 
-    useEffect(() => {
-        console.log("Index mounted");
-        fetchItems();
-    }, [])
+export default function NewsFeed(props: NewsFeedProps) {
+
+
 
     return (
-        <div>
-            <Navbar/>
-            <Items state={state} element={(itemId) => {
-                // @ts-ignore
-                // eslint-disable-next-line react/jsx-key
-                return <ItemHOC item={{id: itemId}}/>
-            }}/>
-
+        <div className={"news-feed"}>
+            {
+                props.items.map((item) => {
+                    return <ItemComponent item={item} key={item.id}/>
+                })
+            }
         </div>
     )
 }
 
+function ItemComponent(props: { item: Item }) {
+    return (
+        <div className={"item-component"}>
+            <span style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "baseline"
+            }}>
+                <h3><a
+                    style={{textDecoration: "none",}}
+                    href={props.item.url}>
+                        {props.item.title}
+                </a></h3>
+                <span style={{marginLeft: "1em"}}>
+                     <a href={props.item.url}>
+                        {getBaseUrl(props.item.url)}
+                    </a>
+                </span>
 
-function Footer() {
-    return <div className={".footer"}>
-        Made with love by <a href={"https://rpi.edu/~/brulel"}>Lucien Brule</a>
-    </div>;
+            </span>
+            <span style={
+                {
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "baseline",
+                    justifyContent: "space-between"
+
+                }}>
+                <div>By: {props.item.by}</div>
+                <div>Score: {props.item.score}</div>
+            </span>
+        </div>
+    )
+}
+
+function getBaseUrl(url: String) {
+    if(!url){
+        return "";
+    }
+    try{
+
+        return url.split("/")[2];
+    }
+    catch (e){
+        console.error(e)
+    }
 }
