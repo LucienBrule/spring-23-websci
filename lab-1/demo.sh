90594c382c7657e94b42784449f705a0d6755de7
Lucien Brule <lucien@lucienbrule.com>
2023-01-20 09:28:02 -0500

    update demo.sh

--- lab-1/demo.sh
+++ lab-1/demo.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # demo.sh - script that calls an api and calls another api based on the response
 
-# A one liner that does the same thing
 curl -s https://hacker-news.firebaseio.com/v0/topstories.json \
   | jq -r '.[0:10]' \
   | tr -d '[],' \
