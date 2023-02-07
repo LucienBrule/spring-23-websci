8b1a448797450e8897ea30213c899e9ad3912dd3
Lucien Brule <lucien@lucienbrule.com>
2023-02-07 10:21:57 -0500

    node-chat

---
+++ in-class/node-chat/server.js
@@ -0,0 +1,58 @@
+// server init + mods
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+
+// server route handler
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+// connect to mongodb
+var db = mongoose.connection;
+db.on('error', console.error);
+mongoose.connect('mongodb://localhost/mychat');
+
+// mongodb schemas
+var chatMessage = new mongoose.Schema({
+  username: String,
+  message: String
+});
+var Message = mongoose.model('Message', chatMessage);
+
+// user connected even handler
+io.on('connection', function(socket){
+  
+  // log & brodcast connect event
+  console.log('a user connected');
+  
+  // log disconnect event
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  
+  // message received event handler
+  socket.on('message', function(msg){
+    // log chat msg
+    console.log('message: ' + msg);
+    
+    // broadcast chat msg to others
+    socket.broadcast.emit('message', msg);
+    
+    // save message to db
+    var message = new Message ({
+      message : msg
+    });
+    message.save(function (err, saved) {
+      if (err) {
+	return console.log('error saving to db');
+      }
+    })
+  });
+});
+
+// start server
+http.listen(3000, function(){
+  console.log('Server up on *:3000');
+});
---
+++ lab-2-scratch/README.md
@@ -0,0 +1,7 @@
+# Lab 2
+
+
+## Citations
+
+Node static server implementation base: [MDN Documentation](https://developer.mozilla.org/en-US/docs/Learn/Server-side/Node_server_without_framework)
+
