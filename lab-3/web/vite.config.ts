9ac77a02cd4b0de5c8a5554f5d3af040a9bebf9c
Lucien Brule <lucien@lucienbrule.com>
2023-05-07 18:15:22 -0400

    rel path hardcode

--- lab-3/web/vite.config.ts
+++ lab-3/web/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '',
+  base: '/labs/lab-3',
   plugins: [react()],
   server: {
     strictPort: true,
