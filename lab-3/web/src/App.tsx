f5faff6da6e671870b7f9e72f4358c656868914d
Lucien Brule <lucien@lucienbrule.com>
2023-05-07 18:58:35 -0400

    ingress n tings

--- lab-3/web/src/App.tsx
+++ lab-3/web/src/App.tsx
@@ -1,10 +1,43 @@
+import React from 'react'
 import './App.css'
 
 function App() {
 
+  const [health, setHealth] = React.useState('unknown')
+
+  React.useEffect(() => {
+
+    const getBackendHealthStatus = async () => {
+      const response = await fetch('./api/v1/health')
+      const json = await response.json()
+      return json.status
+    }
+
+
+    const periodicHealthCheck = setInterval(() => {
+      console.log('Checking backend health...')
+      getBackendHealthStatus().then((status) => {
+        console.log(`Backend health is ${status}`)
+        setHealth(status)
+      }).catch((error) => {
+        console.error('Error checking backend health:', error)
+        setHealth('error')
+      })
+    })
+
+    return () => {
+      clearInterval(periodicHealthCheck)
+    }
+
+
+  }, [])
+
   return (
     <div className="App">
-        <h1>Web</h1>
+      <h1>Web</h1>
+      <pre>
+          health status: {health}
+        </pre>
     </div>
   )
 }
